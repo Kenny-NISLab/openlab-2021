b599be079e75cd4874225ebd52636dd7eef140cc
kogepanh <k.hosono1209@gmail.com>
2021-01-25 14:17:15 +0900

    edit: ...

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+import "firebase/auth";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAKF9Cdf7AEB631RMdVlgxBAdY0p9LNGo8",
+	authDomain: "open-nislab.firebaseapp.com",
+	databaseURL: "https://open-nislab-default-rtdb.firebaseio.com",
+	projectId: "open-nislab",
+	storageBucket: "open-nislab.appspot.com",
+	messagingSenderId: "1082535253333",
+	appId: "1:1082535253333:web:c0e4499e6f07a6fa970bdc",
+	measurementId: "G-4T0MBY68DT"
+
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
