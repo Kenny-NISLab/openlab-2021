4c45f5f3f658cc0fce51c06515835979b6853864
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-01-28 05:56:47 +0900

    Change vuex file directory

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-
 import firebase from './firebase.js'
 import vuetify from './plugins/vuetify'
 
@@ -10,7 +8,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   firebase,
   vuetify,
   render: h => h(App)
