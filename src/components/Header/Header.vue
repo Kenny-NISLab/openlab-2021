7512f3a37a8db7d38a33bc0a450c1a03db4f8eda
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-01-30 04:44:48 +0900

    Change Header style

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,12 +1,18 @@
 <template>
   <v-app-bar
-    class="bg"
+    color="primary"
     app
   >
-    <v-app-bar-nav-icon @click="toggle" />
-    <v-toolbar-title>
+    <v-app-bar-nav-icon
+      color="white"
+      @click="toggle"
+    />
+    <v-toolbar-title class="toolbar__title">
       <h1 lang:en>
-        <router-link to="/">
+        <router-link
+          to="/"
+          class="white--text"
+        >
           NISLAB OpenLAB
         </router-link>
       </h1>
@@ -21,6 +27,7 @@
         v-for="menuItem in menuLogined"
         :key="menuItem.name"
         :to="menuItem.path"
+        class="white--text"
       >
         {{ menuItem.name }}
       </v-tab>
@@ -35,6 +42,7 @@
         v-for="menuItem in menuLogouted"
         :key="menuItem.name"
         :to="menuItem.path"
+        class="white--text"
       >
         {{ menuItem.name }}
       </v-tab>
@@ -46,7 +54,6 @@
 import firebase from '@/firebase.js'
 import storeDrawer from '@/store/drawer.js'
 import storeMenu from '@/store/menu.js'
-
 
 export default {
   name: 'Header',
@@ -85,10 +92,20 @@ export default {
 }
 </script>
 
-<style scoped>
-.v-toolbar__title {
-  margin-right: 50px;
-  overflow: visible;
+<style lang="scss" scoped>
+.toolbar {
+  &__title {
+    margin-right: 48px;
+    overflow: visible;
+
+    @media screen and (max-width: 1024px) {
+      position: absolute;
+      left: 50%;
+      padding: 0 !important;
+      margin: 0;
+      transform: translateX(-50%);
+    }
+  }
 }
 
 @media screen and (max-width: 1024px) {
