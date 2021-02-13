731358fc6b75f56cb21aa65fb14a4bec46914689
kogepanh <k.hosono1209@gmail.com>
2021-02-13 12:03:13 +0900

    #38 Add ExtraPage, router, menu

---
+++ src/views/ExtraContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <article aria-label="追加コンテンツ">
+    <h1 class="heading--main">
+      追加コンテンツ
+    </h1>
+    <h2 class="heading--sub">
+      オープンラボ終了後限定の追加コンテンツをお届けします！
+    </h2>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'ExtraContent',
+  components: {},
+  data () {}
+}
+</script>
